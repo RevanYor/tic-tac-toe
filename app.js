3f154e1e8f8c5d7c6d5692e10a3486784e344820
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-08-09 14:24:12 -0400

    some stuff

---
+++ app.js
@@ -0,0 +1,31 @@
+let gameboard = {
+    gameBoard: []
+};
+
+let mark = '';
+
+//Markers
+let xMarker = document.querySelector('#xmarker');
+let oMarker = document.querySelector('#omarker');
+
+xMarker.addEventListener('click', () => {
+    mark = xMarker.innerText;
+})
+
+oMarker.addEventListener('click', () => {
+    mark = oMarker.innerText;
+    console.log(mark);
+})
+
+let box = document.querySelectorAll('.item');
+
+box.forEach(box.addEventListener('click', () => {
+    box.innerText = mark;
+}));
+
+const playerFactory = (marker) => {
+    const hey = () => console.log(marker);
+    return {marker, hey};
+}
+
+const boy = playerFactory(this.marker);

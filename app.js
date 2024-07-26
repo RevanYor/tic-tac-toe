c81a0dc79873180d6bc39a3e39bd6c195ddeb70f
RevanYor <70781398+RevanYor@users.noreply.github.com>
2024-07-26 11:27:55 -0400

    Started from zero

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ let gameboard = {
     gameBoard: []
 };
 
+let start = document.querySelector('#start');
+
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let four = document.querySelector('.four');
+let five = document.querySelector('.five');
+let six = document.querySelector('.six');
+let seven = document.querySelector('seven');
+let eight = document.querySelector('.eight');
+let nine = document.querySelector('.nine');
+
+const arr = [];
+
+for (let i = 1; i <= 9; i++) {
+    arr.push(document.querySelector('.item'));
+
+}
+
 //Markers
 let xMarker = document.querySelector('#xmarker');
 let oMarker = document.querySelector('#omarker');
@@ -15,16 +34,17 @@ oMarker.addEventListener('click', () => {
     mark = oMarker.innerText;
 })
 
+//Board generation
+
 const boxes = document.querySelectorAll('.item');
         boxes.forEach(box => {
             box.addEventListener('click', () => {
                 if (box.innerText === '') { // Prevent overwriting marks
                     box.innerText = mark;
+                    if([one, four, seven] == box.innerText) {
+                        console.log("win");
+                    }
                 }
             });
         });
 
-const playerFactory = (marker) => {
-    const hey = () => console.log(marker);
-    return {marker, hey};
-}
